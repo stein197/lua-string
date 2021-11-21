5f4a14411f32de67fb794a461eb0a49f302c664a
Nail <nil20122013@gmail.com>
2021-11-21 14:11:25 +0500

    Added stubs for all methods

--- test.lua
+++ test.lua
@@ -1,11 +1,24 @@
 require "."
 local luaunit = require "luaunit"
+
 TestString = {
-	["test: split()"] = function ()
-		luaunit.assertEquals(1, 1)
-	end
+	["test: split()"] = function () error("Not implemented") end; -- TODO
+	["test: splitregex()"] = function () error("Not implemented") end; -- TODO
+	["test: trim()"] = function () error("Not implemented") end; -- TODO
+	["test: padstart()"] = function () error("Not implemented") end; -- TODO
+	["test: padend()"] = function () error("Not implemented") end; -- TODO
+	["test: iter()"] = function () error("Not implemented") end; -- TODO
+	["test: truncate()"] = function () error("Not implemented") end; -- TODO
+	["test: esc()"] = function () error("Not implemented") end; -- TODO
+	["test: unesc()"] = function () error("Not implemented") end; -- TODO
+	["test: ensurestart()"] = function () error("Not implemented") end; -- TODO
+	["test: ensureend()"] = function () error("Not implemented") end; -- TODO
+	["test: startswith()"] = function () error("Not implemented") end; -- TODO
+	["test: endswith()"] = function () error("Not implemented") end; -- TODO
+	["test: isempty()"] = function () error("Not implemented") end; -- TODO
+	["test: isblank()"] = function () error("Not implemented") end; -- TODO
+	["test: tobool()"] = function () error("Not implemented") end; -- TODO
+	["test: totable()"] = function () error("Not implemented") end; -- TODO
 }
 
-local runner = luaunit.LuaUnit.new()
-runner:setOutputType("text")
-os.exit(runner:runSuite())
+os.exit(luaunit.run())
